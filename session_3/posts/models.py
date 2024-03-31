bfe6353e965a78aaf1c82a49846a29356428fefc
kmh0601 <pongkmh@naver.com>
2024-03-31 19:48:51 +0900

    feat: 4th session practical exercise

--- session_3/posts/models.py
+++ session_3/posts/models.py
@@ -2,8 +2,30 @@ from django.db import models
 
 # Create your models here.
 
-class CodeReviewer(models.Model):
-    name = models.CharField(max_length=5)
-    age = models.IntegerField()
-    major = models.CharField(max_length=20)
-    gitHub = models.CharField(max_length=20)
+# class CodeReviewer(models.Model):
+#     name = models.CharField(max_length=5)
+#     age = models.IntegerField()
+#     major = models.CharField(max_length=20)
+#     gitHub = models.CharField(max_length=20)
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="수정일시", auto_now=True)
+
+    class Meta():
+        abstract = True
+
+class Post(BaseModel):
+
+    CHOICES = (
+        ('DIARY', '일기'),  # db에 저장할 실제 값, 우리 눈에 보이는 값
+        ('STUDY', '공부'),
+        ('ETC', '기타')
+    )
+    
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(verbose_name='제목', max_length=20)
+    content = models.TextField(verbose_name='내용')
+    writer = models.CharField(verbose_name='작성자', max_length=10)
+    category = models.CharField(choices=CHOICES, max_length=20)
