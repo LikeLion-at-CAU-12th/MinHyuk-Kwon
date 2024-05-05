a852b4ce6306c60f363bb16d5eee3c9fb84a6aa3
kmh0601 <pongkmh@naver.com>
2024-05-05 22:02:10 +0900

    feat: add CommentSerializer

--- session_3/posts/serializers.py
+++ session_3/posts/serializers.py
@@ -13,5 +13,11 @@ class PostSerializer(serializers.ModelSerializer):
         # # 원하지 않는 필드를 지정할 수 있음
         # exclude = ['category'] 
         # # 수정 불가능 옵션으로 필드를 가져올 수 있음
+        
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        #fields = "__all__"
+        exclude = ['post']
 
         
